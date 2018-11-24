bd7d9e44cb0e1f6717f855a458fa8071b6ac8be2
Vincent Bernat <vincent@bernat.ch>
2018-11-24 12:34:25 +0100

    network: add web04

--- network.nix
+++ network.nix
@@ -23,6 +23,15 @@ in
       address = "fe80::1"; interface = "ens3";
     };
   };
+  web04 = web "hetzner" 4 // {
+    # Static IPv6 configuration
+    networking.interfaces.ens3.ipv6.addresses = [
+      { address = "2a01:4f8:1c0c:5eb5::1"; prefixLength = 64; }
+    ];
+    networking.defaultGateway6 = {
+      address = "fe80::1"; interface = "ens3";
+    };
+  };
   znc = server "exoscale" "znc.luffy.cx" [ ./znc.nix ] // {
     swapDevices = [{
       # Only 512M of RAM, add swap.
