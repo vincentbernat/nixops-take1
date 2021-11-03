af657672935cdda90ca2411810e6eebd4a3b2637
Vincent Bernat <vincent@bernat.ch>
2021-11-03 20:37:01 +0100

    network: add web05

--- network.nix
+++ network.nix
@@ -34,4 +34,15 @@ in {
       interface = "ens3";
     };
   };
+  web05 = web "hetzner" 5 [] // {
+    # Static IPv6 configuration
+    networking.interfaces.enp1s0.ipv6.addresses = [{
+      address = "2a01:4ff:f0:b91::1";
+      prefixLength = 64;
+    }];
+    networking.defaultGateway6 = {
+      address = "fe80::1";
+      interface = "enp1s0";
+    };
+  };
 }
