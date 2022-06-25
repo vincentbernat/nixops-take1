2483c7b79995b51b5c5541d0c2b727dc299d7b7e
Vincent Bernat <vincent@bernat.ch>
2022-06-25 07:31:12 +0200

    nginx: update mailcap patching
    This is now a standard derivation.

--- modules/nginx.nix
+++ modules/nginx.nix
@@ -4,20 +4,15 @@ let
   pkgsWithModifiedMailcap = pkgs // {
     # Update mailcap to replace application/javascript (IANA) with text/javascript (HTML WHATWG).
     # Also fix some others.
-    mailcap = pkgs.mailcap.override {
-      fetchzip = { ... } @ args:
-        pkgs.fetchzip (args // {
-          sha256 = "5n8JlCeXXCTL7aFDyQ+knIgPHJyouDuwulM0dCX3mh4=";
-          postFetch = ''
-            ${args.postFetch}
-            sed -i -e "/^application\/javascript[ \t]/d" \
-                   -e "/^text\/vnd.trolltech.linguist[ \t]/d" \
-                   -e "1a text/javascript js;" \
-                   -e "1a video/mp2t      ts;" \
-                $out/etc/nginx/mime.types
-          '';
-        });
-    };
+    mailcap = pkgs.mailcap.overrideAttrs (old: {
+      postInstall = ''
+        sed -i -e "/^application\/javascript[ \t]/d" \
+               -e "/^text\/vnd.trolltech.linguist[ \t]/d" \
+               -e "1a text/javascript js;" \
+               -e "1a video/mp2t      ts;" \
+            $out/etc/nginx/mime.types
+      '';
+    });
   };
 in
 (import "${modulesPath}/services/web-servers/nginx/default.nix"
