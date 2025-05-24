ccad4756bec4785f1c4abf0f679267714350404c
Vincent Bernat <vincent@bernat.ch>
2025-05-24 09:02:07 +0200

    nix: switch to colmenaHive

--- flake.nix
+++ flake.nix
@@ -2,8 +2,16 @@
   inputs = {
     nixpkgs.url = "github:nixos/nixpkgs/nixos-25.05";
     flake-utils.url = "github:numtide/flake-utils";
+    colmena = {
+      url = "github:zhaofengli/colmena";
+      inputs = {
+        flake-utils.follows = "flake-utils";
+        nixpkgs.follows = "nixpkgs";
+        stable.follows = "nixpkgs";
+      };
+    };
   };
-  outputs = { self, nixpkgs, flake-utils, ... }@inputs:
+  outputs = { self, nixpkgs, flake-utils, colmena, ... }@inputs:
     flake-utils.lib.eachDefaultSystem
       (system:
         let
@@ -16,11 +24,12 @@
               pkgs.curl
               pkgs.colordiff
               pkgs.wdiff
-              pkgs.colmena
+              colmena.packages."${system}".colmena
               pkgs.nix
             ];
           };
         }) // {
+      colmenaHive = colmena.lib.makeHive self.outputs.colmena;
       colmena = import ./network.nix {
         inherit inputs;
       };
